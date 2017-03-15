3ed09026f01bdc1c1c353d58fca7f35bff03963f
Víctor Valencia <valencia6x@gmail.com>
2017-03-15 16:20:22 -0400

    New modules added and assets queue

---
+++ src/Modules/Ajax.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: victor
+ * Date: 15/03/17
+ * Time: 02:10 PM
+ */
+
+namespace Experiensa\Plugin\Modules;
+
+
+class Ajax
+{
+
+}
---
+++ src/Modules/Defaults.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: victor
+ * Date: 15/03/17
+ * Time: 01:56 PM
+ */
+
+namespace Experiensa\Plugin\Modules;
+
+
+class Defaults
+{
+
+}
